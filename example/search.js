fe795955f6afee52f360306b59ef4e53e8f379fa
Stavros Zavrakas <stavros.zavrakas@healthera.co.uk>
2017-04-07 17:59:28 +0100

    refactor search

--- example/search.js
+++ example/search.js
@@ -21,7 +21,7 @@ debugger;
 // });
 
 
-completer_gps.search("dd", 15, function (err, res) {
+completer_gps.search('HEATHFORD GROUP', 10, function (err, res) {
   if (err) {
     console.log(err);
     return;
