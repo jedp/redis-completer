6954d56a72639a77d86d13f8971dd5da3aa37235
Stavros Zavrakas <stavros.zavrakas@healthera.co.uk>
2017-04-07 17:04:52 +0100

    split importer

--- example/imports.js
+++ example/imports.js
@@ -1,9 +1,9 @@
-var Completer = require('../index');
+var Importer = require('../lib/Importer');
 
-var completer_gps = new Completer('gps');
+var importer_gps = new Importer('gps');
 
-// completer_gps.addCompletions("Surgery for humans", 1);
-// completer_gps.addCompletions("Surgery for pets", 2);
-// completer_gps.addCompletions("Surgery for bears", 3);
+// importer_gps.addCompletions("Surgery for humans", 1);
+// importer_gps.addCompletions("Surgery for pets", 2);
+// importer_gps.addCompletions("Surgery for bears", 3);
 
-completer_gps.addFromFile(`${__dirname}/epraccur.csv`);
+importer_gps.addFromFile(`${__dirname}/epraccur.csv`);
