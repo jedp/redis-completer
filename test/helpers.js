add99a44045f3b75e1946df654a0137ae82ff8f9
Stavros Zavrakas <stavros.zavrakas@healthera.co.uk>
2017-04-10 12:12:17 +0100

    first unit test

---
+++ test/helpers.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const expect = require('chai').expect;
+
+const helpers = require('../lib/helpers');
+
+describe('helpers', () => {
+
+  it('should not crash if line does not exits', () => {
+    const shop = helpers.parseLine();
+
+    expect(shop).to.be.an('object');
+    expect(shop.id).to.be.a('string');
+    expect(shop.phrase).to.be.an('undefined');
+    expect(shop.id).to.equal('');
+  });
+
+  it('should return a shop object', () => {
+    const shop = helpers.parseLine('"123","ZARA"');
+
+    expect(shop).to.be.an('object');
+    expect(shop.id).to.be.a('string');
+    expect(shop.phrase).to.be.a('string');
+    expect(shop.id).to.equal('123');
+    expect(shop.phrase).to.equal('ZARA');
+  });
+
+  it('should return a shop object if more data passed', () => {
+    const shop = helpers.parseLine('"123","ZARA","address"');
+
+    expect(shop).to.be.an('object');
+    expect(shop.id).to.be.a('string');
+    expect(shop.phrase).to.be.a('string');
+    expect(shop.id).to.equal('123');
+    expect(shop.phrase).to.equal('ZARA');
+  });
+
+});
